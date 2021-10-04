0a3a5d062ff671fef0b41c9b4556c3e3d206064a
Lucas Amorim <lorenarodrigues673@gmail.com>
2021-10-03 23:58:34 -0300

    atv1

---
+++ atv1/Components/Aluno.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { text, View } from 'react-native'
+
+function Aluno (){
+    return(
+
+        <View>
+            <Text style={{color:'pink', fontweight:'bold'}}> Lorena Amaro Rodrigues</Text>
+            <Text> 21 anos</Text>
+            <Text> Mombaça-CE</Text>
+            <Text> Design Digital</Text>
+   
+        </View>
+    )
+
+}
+
+
+export default Aluno
