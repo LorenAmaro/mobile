ccb93c6a639d5ebe3709e3d54fd59d5df99f823f
Lucas Amorim <lcsamorim503@gmail.com>
2021-11-28 21:19:32 -0300

    atv3

---
+++ atv3/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import IMCApp from './src/components/IMCApp';
+
+export default function App() {
+  return (
+
+    <IMCApp />
+    
+
+    /*<View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>*/
+  );
+}
