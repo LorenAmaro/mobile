a1d7d0746b1fd4c5c2178732fb97c763d8b033e1
LorenAmaro <lorenarodrigues673@gmail.com>
2021-12-12 22:42:22 -0300

    atv4

---
+++ atv4/components/Sobre.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default class Sobre extends Component {
+
+    render(){        
+        return(
+            <View>
+               <Text style={{fontWeight:"bold", fontSize:20}}>
+                   Nunca deixe o medo de perder impedir que voce jogue! 
+                </Text>
+            </View>
+        )
+    }
+}
